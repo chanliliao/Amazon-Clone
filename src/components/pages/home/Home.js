6a0d29f3d75756a5bb503662a1ff78334451d15a
Chan Li Liao <cliao119@gmail.com>
2020-11-19 23:06:42 -0500

    edit product

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -51,7 +51,22 @@ function Home() {
           />
         </div>
 
-        <div className='product-row'></div>
+        <div className='product-row'>
+          <Product
+            id='5'
+            title='Iphone 12'
+            price={999}
+            image='https://images-na.ssl-images-amazon.com/images/G/01/omaha/images/merch/2020/Events-Holiday-SeasonalStores/Thanksgiving/F3VX-2621-Fresh_F3_US_EN_GW_TallHero_06_browser_1x._CB417380239_.jpg'
+            rating={5}
+          />
+          <Product
+            id='5'
+            title='Iphone 12'
+            price={999}
+            image='https://images-na.ssl-images-amazon.com/images/G/01/omaha/images/merch/2020/Events-Holiday-SeasonalStores/Thanksgiving/F3VX-2621-Fresh_F3_US_EN_GW_TallHero_06_browser_1x._CB417380239_.jpg'
+            rating={5}
+          />
+        </div>
       </div>
     </div>
   );
